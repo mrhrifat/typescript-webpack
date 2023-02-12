7d23f3071fe11336b1add9fa86d6ce4e3e52bea3
mrhrifat <mrhrifat383759@gmail.com>
2023-02-12 19:30:48 +0600

    build: 3 imams & array of it

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import Imam from "./lib/class";
 import ImamInterface from "./lib/interface";
 
 console.log("Hello Programmers");
-const imam: ImamInterface = new Imam(1, "Ibn Jawji", 67, "Bagdad");
-console.log(imam);
-imam.seeImam();
+const imams: object[] = [];
 
-// const imam2: ImamInterface = new Imam(2, "Ibn Kayyim", "Iraq", 34); // Not allow cause of "Iraq" stay is wrong position
-// const imam2: ImamInterface = new Imam(2, "Ibn Kayyim", 68, "Iraq", 34); // Not allow cause of 5 parameter instead of 4
-const imam2: ImamInterface = new Imam(2, "Ibn Kayyim", 68, "Iraq"); // Not allow cause of 5 parameter instead of 4
-console.log(imam2);
+const imam1: ImamInterface = new Imam(1, "Ibn Jawji", 67, "Bagdad", "ASWJ");
+console.log({ imam1 });
+imam1.seeImam();
+imams.push(imam1);
+
+// const imam2: ImamInterface = new Imam(2, "Ibn Kayyim", "Iraq", 34, "ASWJ"); // Not allow cause of "Iraq" stay is wrong position
+// const imam2: ImamInterface = new Imam(2, "Ibn Kayyim", 68, "Iraq", 34, "ASWJ"); // Not allow cause of 6 parameter instead of 5
+
+let imam2: ImamInterface = new Imam(2, "Ibn Kayyim", 68, "Iraq", "ASWJ");
+imams.push(imam2);
+console.log({ imam2 });
+imam2.seeImam();
+imam2.seeMajhab();
+
+const imam3: ImamInterface = new Imam(3, "Ibn Kasir", 78, "Iraq", "ASWJ");
+imams.push(imam3);
+
+console.log(imams);
