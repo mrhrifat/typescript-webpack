173f8d750b08fbb64b7daffb631e3fb5c0e0df28
mrhrifat <mrhrifat383759@gmail.com>
2023-02-12 19:31:08 +0600

    build: private property interface & method

--- src/lib/class.ts
+++ src/lib/class.ts
@@ -1,13 +1,18 @@
 import ImamInterface from "./interface";
 export default class Imam implements ImamInterface {
   constructor(
-    readonly id: number | string,
+    public id: number | string,
     readonly name: string,
     readonly age: number,
-    readonly country: string
+    readonly country: string,
+    private majhab: string
   ) {}
 
   seeImam(): void {
     console.log(`Imam ${this.name} is born in ${this.country}`);
+  }
+
+  seeMajhab() {
+    return this.majhab;
   }
 }
--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -1,10 +1,13 @@
 // Interface to create class
 export default interface ImamInterface {
   id: number | string;
-  name: string;
-  country: string;
-  age: number;
+  readonly name: string;
+  readonly country: string;
+  readonly age: number;
 
   // seeImam return void
   seeImam(): void;
+
+  // Return private property
+  seeMajhab(): string;
 }
